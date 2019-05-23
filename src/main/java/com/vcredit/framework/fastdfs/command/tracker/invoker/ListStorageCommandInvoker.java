d2c60adb6da09c0e4230149dfee137cc8cfb4f25
tangx <tangxu@os.vcredit.com>
2019-05-23 17:03:44 +0800

    补添加listStorage指令Invoker

---
+++ src/main/java/com/vcredit/framework/fastdfs/command/tracker/invoker/ListStorageCommandInvoker.java
@@ -0,0 +1,51 @@
+/*
+ *    Copyright 2019 VCREDIT
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.vcredit.framework.fastdfs.command.tracker.invoker;
+
+import com.vcredit.framework.fastdfs.command.BaseOperationResult;
+import com.vcredit.framework.fastdfs.command.ProtoHead;
+import com.vcredit.framework.fastdfs.command.tracker.TrackerCommand;
+import com.vcredit.framework.fastdfs.command.tracker.request.TrackerListStorageRequest;
+import com.vcredit.framework.fastdfs.command.tracker.result.StorageState;
+import com.vcredit.framework.fastdfs.command.tracker.result.StorageStateResult;
+import com.vcredit.framework.fastdfs.exception.InvokeCommandException;
+import com.vcredit.framework.fastdfs.util.ProtoPackageUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.List;
+
+/**
+ * @author tangxu
+ */
+public class ListStorageCommandInvoker extends AbstractTrackerCommandInvoker {
+
+    public ListStorageCommandInvoker(TrackerCommand.ListStorage command) {
+        this.command = command;
+        super.request = new TrackerListStorageRequest(command.getGroupName(), command.getStorageIpAddr());
+    }
+
+    @Override
+    protected BaseOperationResult parseContent(InputStream in, ProtoHead head, Charset charset) throws InvokeCommandException, IOException {
+        byte[] content = ProtoPackageUtil.recvResponseBody(in, head.getContentLength());
+        List<StorageState> list = packageList(StorageState.class, content, charset);
+        return new StorageStateResult(list);
+    }
+
+
+}
